980d8e00142e131f6a27f838f6b08d157aa48ff9
Fahi-alt <73867042+Fahi-alt@users.noreply.github.com>
2021-07-14 08:41:55 +0600

    Update creds.py

--- creds.py
+++ creds.py
@@ -1,6 +1,6 @@
 class Creds():
     # ENTER Your bot Token Here
-    TG_TOKEN = ""
+    TG_TOKEN = "1647728886:AAECt8xQLEo0XKqWNDZ51yBnAb_pxFunvq8"
     
     
     
@@ -8,10 +8,10 @@ class Creds():
     # Because of pydrive And pydrive v2 Api
     
     #Folder Id Of Teamdrive
-    TEAMDRIVE_FOLDER_ID = ""
+    TEAMDRIVE_FOLDER_ID = "1Bzd2OtwDtf9JjIUewb6kPqRVeVZI-_gh"
     
     # Id of Team drive 
-    TEAMDRIVE_ID = ""
+    TEAMDRIVE_ID = "0ALG5g1jnCtjBUk9PVA"
     
     
     
